19cb65c9c01b671f9681c35801cab824219cf2c8
kyorohiro <kyorohiro@gmail.com>
2015-04-21 13:43:17 +0900

    add comment

--- web/test001.dart
+++ web/test001.dart
@@ -4,10 +4,12 @@ import 'dart:web_gl' as gl;
 void main() {
   var canvas = new html.CanvasElement(width: 500, height: 500);
   html.document.body.append(canvas);
+
   gl.RenderingContext GL = canvas.getContext3d();
-  int w = canvas.width;
-  int h = canvas.height;
-  GL.clearColor(0.6, 0.2, 0.2, 1.0);
-  GL.viewport(0, 0, w, h);
+  double r = 0.6;
+  double g = 0.2;
+  double b = 0.2;
+  double a = 1.0;
+  GL.clearColor(r, g, b, a);
   GL.clear(gl.RenderingContext.COLOR_BUFFER_BIT);
 }
