2a0aca7a9e64e12b2ee48926cf1559ec9d9c5df9
kyorohiro <kyorohiro@gmail.com>
2015-08-06 14:42:33 +0900

    add draw rect sample

--- web/draw_rect.dart
+++ web/draw_rect.dart
@@ -8,24 +8,18 @@ void main() {
   html.document.body.append(canvas);
 
   webgl.RenderingContext GL = canvas.getContext3d();
-  double r = 0.6;
-  double g = 0.2;
-  double b = 0.2;
-  double a = 1.0;
-  GL.clearColor(r, g, b, a);
+  GL.clearColor(0.6, 0.2, 0.2, 1.0);
   GL.clear(webgl.RenderingContext.COLOR_BUFFER_BIT);
-  //
+
   // setup shader
-  //
-  webgl.Shader vertexShader = hetimagl.loadShader(GL,
-      webgl.RenderingContext.VERTEX_SHADER, 
+  webgl.Shader vertexShader = loadShader(GL, webgl.RenderingContext.VERTEX_SHADER, 
           "attribute vec3 vp;\n" +
           "void main() {\n" +
           "  gl_Position = vec4(vp, 1.0);\n" +
           "}\n");
 
-  webgl.Shader fragmentShader = hetimagl.loadShader(GL,
-      webgl.RenderingContext.FRAGMENT_SHADER, "precision mediump float;\n" +
+  webgl.Shader fragmentShader = loadShader(GL, webgl.RenderingContext.FRAGMENT_SHADER, 
+          "precision mediump float;\n" +
           "void main() {\n" +
           " gl_FragColor = vec4(0.0,1.0,0.0,1.0);\n" +
           "}\n");
@@ -36,14 +30,14 @@ void main() {
   GL.linkProgram(shaderProgram);
   GL.useProgram(shaderProgram);
 
-  if (false ==
-      GL.getProgramParameter(
-          shaderProgram, webgl.RenderingContext.LINK_STATUS)) {
+  if (false == GL.getProgramParameter(shaderProgram, webgl.RenderingContext.LINK_STATUS)) {
     String message = "alert: Failed to linked shader";
     throw new Exception("${message}\n");
   }
 
-  // 左上 (x, y, z), 左下, 右上, 右下
+  //
+  // setup
+  // leftup (x, y, z), leftdown, rightup, rightdown
   data.TypedData rectData = new data.Float32List.fromList([-0.8, 0.8, 0.0, -0.8, -0.8, 0.0, 0.8, 0.8, 0.0, 0.8, -0.8, 0.0]);
   data.TypedData rectDataIndex = new data.Uint16List.fromList([0, 1, 2, 1, 3, 2]);
 
@@ -55,8 +49,24 @@ void main() {
   GL.bindBuffer(webgl.ELEMENT_ARRAY_BUFFER, rectIndexBuffer);
   GL.bufferDataTyped(webgl.RenderingContext.ELEMENT_ARRAY_BUFFER, rectDataIndex, webgl.RenderingContext.STATIC_DRAW);
 
+  //
+  // draw
   int locationVertexPosition = GL.getAttribLocation(shaderProgram, "vp");
   GL.vertexAttribPointer(locationVertexPosition, 3, webgl.RenderingContext.FLOAT, false, 0, 0);
   GL.enableVertexAttribArray(locationVertexPosition);
   GL.drawElements(webgl.RenderingContext.TRIANGLES, 6, webgl.RenderingContext.UNSIGNED_SHORT, 0);
 }
+
+webgl.Shader loadShader(webgl.RenderingContext context, int type, var src) {
+  webgl.Shader shader = context.createShader(type);
+  context.shaderSource(shader, src);
+  context.compileShader(shader);
+  if (false == context.getShaderParameter(shader, webgl.RenderingContext.COMPILE_STATUS)) {
+    String message = "Error compiling shader ${context.getShaderInfoLog(shader)}";
+    throw new Exception("${message}\n");
+    context.deleteShader(shader);
+    return null;
+  }
+  return shader;
+}
+
